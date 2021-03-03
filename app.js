a5828de37c14295206eb16fd84c621c8042a5c66
Elmakhlouk Chouaib <elchouai@e1r6p13.1337.ma>
2021-03-03 15:06:48 +0100

    setup and initialize the node server

---
+++ app.js
@@ -0,0 +1,13 @@
+//imports 
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+
+
+
+// Listen on port 3000
+app.listen(port, ()=> console.info('Listening on port '+port))
